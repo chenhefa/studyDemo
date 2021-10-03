7d62a05bb2256b1f58e53ea4d3b4a5d2b3e6167b
陈合发 <2275719785@qq.com>
2021-10-03 15:03:34 +0800

    bug修复了

---
+++ main.js
@@ -0,0 +1,18 @@
+let cp = {
+    0: "路飞",
+    1: "女帝"
+}
+let str = ""
+for (let name in cp) {
+    str += name + ":" + cp[name] + "\n"
+}
+console.log(str)
+
+function add(a, b) {
+    if (a != undefined && b != undefined) {
+        return a + b
+    } else {
+        return 0
+    }
+}
+console.log(add(a: 1), "输出结果为NaN");
